29ed7c34725e9e000c7d36e2f9724857bb5fc6ac
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2019-02-25 22:43:05 -0800

    Add awgn.

--- wavegen.py
+++ wavegen.py
@@ -7,15 +7,25 @@ This is a temporary script file.
 
 import numpy as np
 
-def sine_wave(freq, duration=0.05, sampling=1/10000):
+def sine_wave(freq, duration=0.05, Ts=1/10000):
     '''
     inputs: 
         freq (Hz)
         duration (sec)
-        sampling (Hz)
+        sampling duration (sec)
     outputs:
         timestep, sine waveform
     '''
-    t = np.arange(0, duration, sampling)
+    t = np.arange(0, duration, Ts)
     w = np.sin(2*np.pi*freq*t)
     return t, w
+
+
+def awgn(duration=0.05, Ts=1/10000):
+    '''
+    Returns an awgn waveform
+    inputs:
+        duration (sec)
+        sampling duration
+    '''
+    return np.random.randn(int(duration/Ts))
