29079b649cd564fe9bf803f1816053ab3a913ec4
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2019-12-14 01:57:53 -0800

    Add exponential and gaussian pulses

--- wavegen.py
+++ wavegen.py
@@ -52,3 +52,21 @@ def mysinc(freq, duration=0.05, Ts=1/10000):
     t = np.arange(0, duration, Ts)
     w = np.sinc(2*freq*(t-duration/2))
     return t, w
+
+
+def exp_pulse(duration=0.05, Ts=1/10000):
+    '''
+    Creates an exponential pulse e^-t
+    '''
+    t = np.arange(0, duration, Ts)
+    pulse = np.exp(-t)
+    return t, pulse
+
+
+def gaussian_pulse(start, stop, Ts=1/10000):
+    '''
+    Creates an exponential pulse e^-(t^2)
+    '''
+    t = np.arange(start, stop, Ts)
+    pulse = np.exp(-(t**2))
+    return t, pulse
