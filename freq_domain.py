f5cb74fc8033d44f10e58e1f92ebc6bc6c9b003d
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2019-12-14 10:41:30 -0800

    Simplify plotspec. Remove axis parameter

--- freq_domain.py
+++ freq_domain.py
@@ -17,10 +17,9 @@ import global_params as G
 
 def plot_signal(t, sig, ax_sig):
     # Plot signal
-    if ax_sig is not None: 
-        ax_sig.plot(t, sig)
-        ax_sig.set_xlabel('Seconds')
-        ax_sig.set_ylabel('Amplitude')
+    ax_sig.plot(t, sig)
+    ax_sig.set_xlabel('Seconds')
+    ax_sig.set_ylabel('Amplitude')
     return ax_sig
 
 
@@ -32,20 +31,12 @@ def plot_amplitude(freq_range, amplitude, ax_spec):
     return ax_spec
 
 
-def plot_phase(freq_range, phase, ax_spec):
-    # Plot spectrum phase
-    ax_spec.plot(freq_range, phase)
-    ax_spec.set_xlabel('Frequency')
-    ax_spec.set_ylabel('Phase')
-
-
-def plotspec(sig, Ts, ax_spec=None):
+def plotspec(sig, Ts):
     '''
     Returns the spectrum centered at 0
     inputs:
         sig: signal
         Ts: sampling interval (in sec)
-        ax_spec: axis to plot spectrum
     '''
     N = len(sig)
     t = np.arange(0, N)*Ts
@@ -53,16 +44,11 @@ def plotspec(sig, Ts, ax_spec=None):
     freq_range = np.arange(math.ceil(-N/2), math.ceil(N/2))/(Ts*N) # Frequency vector
     fx = fft(sig) # FFT
 
-    freqs = fftshift(fx) # Shift it for plotting
-    amplitude = np.abs(freqs)
-    phase = np.unwrap( np.angle(freqs) )
+    spectrum = fftshift(fx) # Shift it for plotting
+    amplitude = np.abs(spectrum)
 
-    if ax_spec is None:
-        _, (ax_sig, ax_spec) = plt.subplots(1,2, figsize=G.FIGSIZE )
-    else:
-        ax_sig = None # If spectrum axis is given, no need to plot signal
-
-    plot_signal(t, sig, ax_sig)
-    ax_spec = plot_amplitude(freq_range, amplitude, ax_spec)
-    return ax_spec
-
+    _, ax = plt.subplots(1,2, figsize=G.FIGSIZE )
+    
+    plot_signal(t, sig, ax[0])
+    plot_amplitude(freq_range, amplitude, ax[1])
+    return ax
