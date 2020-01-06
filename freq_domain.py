cc24755bdd07540b7dc716a1ecd841267c7fd890
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-01-06 07:20:55 -0800

    Add phase spectrum plotting

--- freq_domain.py
+++ freq_domain.py
@@ -31,12 +31,21 @@ def plot_amplitude(freq_range, amplitude, ax_spec):
     return ax_spec
 
 
-def plotspec(sig, Ts):
+def plot_phase(freq_range, phase, ax_spec):
+    # Plot spectrum phase
+    ax_spec.plot(freq_range, phase)
+    ax_spec.set_xlabel('Frequency')
+    ax_spec.set_ylabel('Phase')
+    return ax_spec
+
+
+def plotspec(sig, Ts, amp=True):
     '''
     Returns the spectrum centered at 0
     inputs:
         sig: signal
         Ts: sampling interval (in sec)
+        amp: amplitude if True, else phase
     '''
     N = len(sig)
     t = np.arange(0, N)*Ts
@@ -46,10 +55,14 @@ def plotspec(sig, Ts):
 
     spectrum = fftshift(fx) # Shift it for plotting
     amplitude = np.abs(spectrum)
+    phase = np.unwrap( np.angle(spectrum) )
 
     fig, ax = plt.subplots(1,2, figsize=G.FIGSIZE )
     
     plot_signal(t, sig, ax[0])
-    plot_amplitude(freq_range, amplitude, ax[1])
+    if amp:
+        plot_amplitude(freq_range, amplitude, ax[1])
+    else:
+        plot_phase(freq_range, phase, ax[1])
     fig.subplots_adjust(wspace=0.3)
     return ax
