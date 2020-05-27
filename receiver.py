1a51eee1608d243cd1108b5c0d2dec70326e76a4
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-05-27 10:46:20 -0700

    Create demodulator block

--- receiver.py
+++ receiver.py
@@ -40,10 +40,19 @@ def demodulate(sig, fc, Ts):
         fc : carrier wave frequency
         Ts : sampling duration of analog signal
     '''
+    # Downconvert
     duration = len(sig)*Ts
-    t, carrier = cosine_wave(fc, duration, Ts)
-    demodulated = sig * carrier
-    return t, demodulated
+    _, carrier = cosine_wave(fc, duration, Ts)
+    downconverted = sig * carrier
+    # Low pass filter the downconverted signal
+    taps = 50
+    Fs = 1/Ts
+    band_edges = np.array([0, 0.1, 0.2, 1])*(Fs//2) # Cutoff at 0.2*Fs/2
+    damps = [1, 0]
+    b = signal.remez(taps, band_edges, damps, fs=Fs)
+    # Scaling by 2 below to compensate for cos(x)**2 = (1/2)*[cos(2x) + 1]
+    baseband = 2*signal.lfilter(b, 1, downconverted) # Baseband is still in 'analog' domain
+    return baseband
 
 
 # Final receiver
@@ -54,18 +63,11 @@ def ideal_receiver(sig):
     '''
     fc = 20
     Ts = 1/100
-    # Demodulate the carrier wave
-    _, demodulated = demodulate(sig, fc, Ts)
-    # low pass filter
-    taps = 50
-    Fs = 1/Ts
-    band_edges = np.array([0, 0.1, 0.2, 1])*(Fs//2) # Cutoff at 0.1*Fs/2
-    damps = [1, 0]
-    b = signal.remez(taps, band_edges, damps, fs=Fs)
-    # Scaling by 2 below to compensate for cos(x)**2 = (1/2)*[cos(2x) + 1]
-    baseband = 2*signal.lfilter(b, 1, demodulated) # Baseband is still in 'analog' domain
-    # Use correlation to extract pulse amplitudes
     oversample_factor = 100
+    # Demodulate the carrier wave
+    baseband = demodulate(sig, fc, Ts)
+
+    # Use correlation to extract pulse amplitudes
     pulse = signal.hamming(oversample_factor)
     pulse_normalized = pulse/(power(pulse)*len(pulse))
     correlator = np.correlate(baseband, pulse_normalized, 'full')
