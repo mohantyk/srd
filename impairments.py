cf53720a93b9bb7504da341efacaed9d387caf75
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-05-28 10:13:11 -0700

    Implement AGC

--- impairments.py
+++ impairments.py
@@ -7,3 +7,14 @@ def time_varying_fading_channel(sig):
     channel[fading_start:] = 0.5
     faded_sig = sig*channel
     return faded_sig
+
+
+def agc(sig, desired_power, learning_rate=0.0003):
+    gains = np.zeros_like(sig)
+    output = np.zeros_like(sig)
+    gains[0] = 1
+    for i, sample in enumerate(sig[:-1]):
+        output[i] = gains[i] * sample
+        gains[i+1] = gains[i] - learning_rate*(output[i]**2 - desired_power)
+    output[-1] = gains[-1] * sig[-1]
+    return output, gains
