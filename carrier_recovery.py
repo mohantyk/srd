a6fa3928b5216a44a69c0157e06a76d1347271b4
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-05-29 23:25:28 -0700

    Implement squared diff estimation function

--- carrier_recovery.py
+++ carrier_recovery.py
@@ -18,3 +18,29 @@ def estimate_carrier_with_fft(sig, Ts):
     freq = freq_range[max_idx]
     phase = np.angle(sig_fft[max_idx])
     return freq, phase
+
+
+def pll_sq_diff(sig, freq_estimate, Ts, mu=0.001, window=25, initial_ph_estimate=0):
+    '''
+    inputs:
+        sig: signal at double the carrier frequency (and double the phase of carrier)
+        freq_estimate: estimate of carrier frequency (assumed to be known perfectly)
+        Ts: sampling duration
+        mu: learning rate
+        window: width of window over which averaging is done
+    output:
+        adaptive estimates of phase
+    '''
+    f0 = freq_estimate
+    theta = np.zeros_like(sig)
+    theta[0] = initial_ph_estimate
+    avg_filter = np.ones(window)/window
+    buffer = np.zeros_like(avg_filter)
+
+    for k, sample in enumerate(sig[:-1]):
+        tick = k*Ts
+        deriv = (sample - np.cos(4*np.pi*f0*tick + 2*theta[k])) * np.sin(4*np.pi*f0*tick + 2*theta[k])
+        buffer = np.roll(buffer, 1)
+        buffer[0] = deriv
+        theta[k+1] = theta[k] - mu*np.dot(avg_filter, buffer)
+    return theta
