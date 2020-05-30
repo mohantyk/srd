bd9c1499edf45502b018242f7fe3ee195c0d2f6c
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-05-30 11:02:17 -0700

    Implement costas loop

--- carrier_recovery.py
+++ carrier_recovery.py
@@ -78,3 +78,42 @@ def pll(sig, freq_estimate, Ts, mu=0.003, psi=0, initial_phase=0):
         buffer[0] = deriv
         theta[k+1] = theta[k] - mu*np.dot(lpf, buffer)
     return theta
+
+
+def costas(sig, freq_estimate, Ts, mu=0.003, initial_phase=0):
+    '''
+    inputs:
+        sig: original signal (no need to process with non-linearity)
+        freq_estimate: estimate of carrier frequency (assumed to be known perfectly)
+        Ts: sampling duration
+        mu: learning rate
+        initial_phase: initial estimate of phase
+    output:
+        adaptive estimates of phase
+    '''
+    f0 = freq_estimate
+    theta = np.zeros_like(sig)
+    theta[0] = initial_phase
+    # LPF
+    Fs = 1/Ts
+    taps = 500
+    band_edges = np.array([0, 0.01, 0.02, 1])*(Fs/2)
+    damps = [1, 0]
+    lpf = signal.remez(taps, band_edges, damps, fs=Fs)
+    # Two buffers, one each for the cosine path and sine path
+    buffer_cosine = np.zeros_like(lpf)
+    buffer_sine = np.zeros_like(lpf)
+
+    for k, sample in enumerate(sig[:-1]):
+        tick = k*Ts
+        # Cosine path
+        path_cosine = 2*np.cos(2*np.pi*f0*tick + theta[k])*sample
+        buffer_cosine = np.roll(buffer_cosine, 1)
+        buffer_cosine[0] = path_cosine
+        # Sine path
+        path_sine = 2*np.sin(2*np.pi*f0*tick + theta[k])*sample
+        buffer_sine = np.roll(buffer_sine, 1)
+        buffer_sine[0] = path_sine
+        # Update theta
+        theta[k+1] = theta[k] - mu*np.dot(lpf, buffer_cosine)*np.dot(lpf, buffer_sine)
+    return theta
