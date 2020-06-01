02e2f5cd598b28b82cdcb8b509eead13ddf6dd79
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-05-31 20:25:20 -0700

    Move eye diagram code to receiver

--- receiver.py
+++ receiver.py
@@ -80,6 +80,22 @@ def quantalph(sig, alphabet):
     return hard_decisions
 
 
+def eye_diag(analog, n_eye=5, oversample_factor=10):
+    '''
+    inputs:
+        analog: analog signal
+        n_eye: number of symbols for eye
+        oversample_factor: oversampling factor for analog signal
+    output:
+        2D array, each row consists of n_eye symbols
+    '''
+    M = oversample_factor
+    n_eye = 5 # Number of symbols in eye
+    groups = len(analog)//(n_eye*M)
+    eye_diag = analog[-n_eye*groups*M:].reshape(-1, n_eye*M)
+    return eye_diag
+
+
 # Final receiver
 def ideal_receiver(sig, fc=20, Ts=1/100):
     '''
