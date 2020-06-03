32bfc17211161ff464c1c4626d332fc7c593e8fc
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-06-03 15:14:39 -0700

    Add interpolator

--- sampling.py
+++ sampling.py
@@ -1,4 +1,5 @@
 import numpy as np
+from wavegen import srrc
 
 def sample_waveform(sig, fs, Ts):
     '''
@@ -27,3 +28,22 @@ def oversample(sig, factor=10):
     oversampled[::factor] = sig
     return oversampled
 
+
+def interpolate(samples, t, l, β=0):
+    '''
+    Interpolate to find a single point using a sinc (or srrc) waveform
+    inputs:
+        samples : sampled data
+        t       : time at which value is desired
+        l       : one sided length from which to interpolate
+        β       : roll-off factor for srrc (0 is sinc)
+    '''
+    t_now = np.round(t)
+    τ = t - t_now
+    waveform = srrc(l, 1, β, τ)
+
+    lmargin = max(0, t_now-l)
+    rmargin = min(len(samples), t_now+l+1)
+    x = np.convolve(samples[lmargin:rmargin], waveform, 'same')
+    final = x[l]
+    return final
